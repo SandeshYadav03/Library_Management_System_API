e83c6f2131ebc42900bf01726a6282abef47e1a7
Sandesh <sandeshyadav5108@gmail.com>
2022-09-11 19:37:45 +0530

    Second Commit

--- Books/urls.py
+++ Books/urls.py
@@ -2,13 +2,13 @@ from django.urls import path,include
 from . import views
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
-router.register('', views.StudentsView)
 router.register('Books', views.BookView)
 router.register('Category', views.CategoryView)
 router.register('Signup', views.SignupView,basename='test')
+router.register('Students', views.StudentsView)
+
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('login/',views.LoginView.as_view()),
+    path('login/',views.LoginView.as_view())
 ]
-
--- Books/views.py
+++ Books/views.py
@@ -4,12 +4,13 @@ from .serializer import Book_Serializer, Category_Serializer, SignupSerializer
 from django.contrib.auth.models import User
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.authentication import TokenAuthentication
+# from rest_framework.authentication import TokenAuthentication
 
     
 class CategoryView(viewsets.ModelViewSet):
     serializer_class = Category_Serializer
     queryset = Book_Category.objects.all()
+
 
 class BookView(viewsets.ModelViewSet):
     serializer_class = Book_Serializer
@@ -17,6 +18,7 @@ class BookView(viewsets.ModelViewSet):
 
 
 class SignupView(viewsets.ModelViewSet):
+    
     serializer_class = SignupSerializer
     queryset = User.objects.filter(is_superuser=False, is_staff=False)
     
